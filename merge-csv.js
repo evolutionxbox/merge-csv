7408e4b0d4cb22346db3540fd6406980ce5ec230
Jonathan Cousins <evolutionxbox@gmail.com>
2018-05-07 19:00:53 +0100

    Add basic node cli argument parsing

--- merge-csv.js
+++ merge-csv.js
@@ -11,3 +11,25 @@ const requirer = require('./custom_modules/require-custom')(`${__dirname}/custom
 const templateEngine = requirer('template-engine')
 const onlyAlphanumeric = requirer('only-alphanumeric')
 const compose = requirer('compose')
+
+// Node CLI Arguements
+const args = [...process.argv]
+const data = args[2]
+const template = args[3]
+const writeFolder = `${args[4]}/` || `${__dirname}/_merged/`
+
+if (!fs.existsSync(data)) {
+  throw new Error('data file must exist!')
+}
+
+if (!fs.existsSync(template)) {
+  throw new Error('template file must exist!')
+}
+
+if (!fs.existsSync(writeFolder)) {
+  try {
+    fs.mkdirSync(writeFolder)
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err
+  }
+}
