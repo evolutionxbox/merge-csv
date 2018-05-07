30e7f1974888a919ef595a5c563c1a702fd0b4db
Jonathan Cousins <evolutionxbox@gmail.com>
2018-05-07 19:00:07 +0100

    Custom module requires. Including template engine, alpha-numeric sanitiser, and function composer

--- merge-csv.js
+++ merge-csv.js
@@ -5,3 +5,9 @@
 const csv = require('fast-csv')
 const fs = require('fs')
 const camelcase = require('camelcase')
+
+// Custom requires
+const requirer = require('./custom_modules/require-custom')(`${__dirname}/custom_modules`)
+const templateEngine = requirer('template-engine')
+const onlyAlphanumeric = requirer('only-alphanumeric')
+const compose = requirer('compose')
